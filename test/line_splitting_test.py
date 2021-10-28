719d524efc3eeb010ed599f0141fdde25463d7ad
jamesanelay <jamesanelay@gmail.com>
2021-10-28 23:15:12 +0100

    Write failing test for line splitter

--- test/line_splitting_test.py
+++ test/line_splitting_test.py
@@ -1,5 +1,13 @@
 import unittest
 
+
+class LineSplitter:
+    def split(self, param, param1):
+        pass
+
+
 class LineSplittingTest(unittest.TestCase):
-    def test_nothing(self):
-        self.assertTrue(0)
+    def test_spitting_a_line(self):
+        line_splitter = LineSplitter()
+        result = line_splitter.split("One,Two,Three,Four", ",")
+        self.assertEqual(["One", "Two", "Three", "Four"], result)
